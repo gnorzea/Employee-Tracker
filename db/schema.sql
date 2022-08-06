67a3cf0f460b75c9d6e1acadb8de3bbcc91e15de
Thunnder <gnorzea@gmail.com>
2022-08-06 17:59:56 -0400

    initial table

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employee(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+     first_name VARCHAR(30) NOT NULL,
+     last_name VARCHAR(30) NOT NULL,
+     job_title VARCHAR(30) NOT NULL,
+     department VARCHAR(30) NOT NULL,
+     salary VARCHAR(30) NOT NULL,
+     manager VARCHAR(30) NOT NULL
+     );
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee (first_name, last_name, job_title, department,salary,manager)
+VALUES
+  ('Ronald', 'Firbank', 'Salesperson', 'Sales', '$80,000', 'John Doe'),
+  ('Virginia', 'Woolf', 'Lead Engineer', 'Engineering', '$150,000', 'Jane Doe');
+
+ 
