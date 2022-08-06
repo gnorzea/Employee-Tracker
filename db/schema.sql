eab9058ec0651dc859f17651c2a786f9dc5152a2
Thunnder <gnorzea@gmail.com>
2022-08-06 19:32:11 -0400

    fixed tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,19 @@
-    CREATE TABLE department (
+    DROP TABLE IF EXISTS department;
+    DROP TABLE IF EXISTS roles;
+    DROP TABLE IF EXISTS employee;
+
+    CREATE TABLE department(
         id INTEGER AUTO_INCREMENT PRIMARY KEY,
-        department_name VARCHAR(30) NOT NULL,
+        department_name VARCHAR(30) NOT NULL
         );
 
-    CREATE TABLE role(
+    CREATE TABLE roles(
         id INTEGER AUTO_INCREMENT PRIMARY KEY,
         title VARCHAR(30) NOT NULL,
         salary DECIMAL(10,2) NOT NULL,
         department_id INTEGER NOT NULL,
-        CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
-        )
+        FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+        );
 
 
     CREATE TABLE employee(
@@ -18,8 +22,9 @@
         last_name VARCHAR(30) NOT NULL,
         role_id INTEGER NOT NULL,
         manager_id INTEGER,
-        CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE
-        CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+        FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+        FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
         );
+    
 
    
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,26 +5,26 @@ VALUES
 ('Finance'),
 ('Legal');
 
-INSERT INTO role (title,salary,department_id)
+INSERT INTO roles (title,salary,department_id)
 VALUES
-('Salesperson', '$80,000',1 ),
-('Lead Engineer', '$150,000',2),
-('Software Engineer', '$120,000',2),
-('Account Manager', '$160,000',3),
-('Accountant', '$125,000',3),
-('Legal Team Lead', '$250,000',4),
-('Lawyer', '$190,000',4);
+('Salesperson', 80000,1),
+('Lead Engineer', 150000,2),
+('Software Engineer', 120000,2),
+('Account Manager', 160000,3),
+('Accountant', 125000,3),
+('Legal Team Lead', 250000,4),
+('Lawyer', 190000,4);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('Dora', 'Carrington',1,NULL),
+('Dora', 'Carrington', 1, NULL),
 ('Edward', 'Bellamy', 2, NULL),
 ('Montague', 'Summers', 3, NULL),
 ('Octavia', 'Butler', 4,NULL),
 ('Virginia', 'Woolf', 5, NULL),
 ('Piers', 'Gaveston', 6, NULL),
 ('Charles', 'LeRoi', 7, NULL),
-('Katherine', 'Mansfield', 8, NULL),
+('Katherine', 'Mansfield', 8, NULL);
 
  
