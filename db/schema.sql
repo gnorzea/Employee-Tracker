56366c32fc4c7ce8a7b0913ff2cc28a1b1fa9b95
Thunnder <gnorzea@gmail.com>
2022-08-06 18:37:19 -0400

    created tables-fixed employee

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,25 @@
-CREATE TABLE employee(
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-     first_name VARCHAR(30) NOT NULL,
-     last_name VARCHAR(30) NOT NULL,
-     job_title VARCHAR(30) NOT NULL,
-     department VARCHAR(30) NOT NULL,
-     salary VARCHAR(30) NOT NULL,
-     manager VARCHAR(30) NOT NULL
-     );
+    CREATE TABLE department (
+        id INTEGER AUTO_INCREMENT PRIMARY KEY,
+        department_name VARCHAR(30) NOT NULL,
+        );
+
+    CREATE TABLE role(
+        id INTEGER AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(30) NOT NULL,
+        salary DECIMAL(10,2) NOT NULL,
+        department_id INTEGER NOT NULL,
+        CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
+        )
+
+
+    CREATE TABLE employee(
+        id INTEGER AUTO_INCREMENT PRIMARY KEY,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        role_id INTEGER NOT NULL,
+        manager_id INTEGER,
+        CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE
+        CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+        );
+
+   
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,30 @@
-INSERT INTO employee (first_name, last_name, job_title, department,salary,manager)
+INSERT INTO department (department_name) 
+VALUES 
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
+
+INSERT INTO role (title,salary,department_id)
 VALUES
-  ('Ronald', 'Firbank', 'Salesperson', 'Sales', '$80,000', 'John Doe'),
-  ('Virginia', 'Woolf', 'Lead Engineer', 'Engineering', '$150,000', 'Jane Doe');
+('Salesperson', '$80,000',1 ),
+('Lead Engineer', '$150,000',2),
+('Software Engineer', '$120,000',2),
+('Account Manager', '$160,000',3),
+('Accountant', '$125,000',3),
+('Legal Team Lead', '$250,000',4),
+('Lawyer', '$190,000',4);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Dora', 'Carrington',1,NULL),
+('Edward', 'Bellamy', 2, NULL),
+('Montague', 'Summers', 3, NULL),
+('Octavia', 'Butler', 4,NULL),
+('Virginia', 'Woolf', 5, NULL),
+('Piers', 'Gaveston', 6, NULL),
+('Charles', 'LeRoi', 7, NULL),
+('Katherine', 'Mansfield', 8, NULL),
 
  
